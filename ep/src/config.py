7650db1b7b9806b9ac4cbf575bd81668515dcb46
yashalgo <yash.iitd21@gmail.com>
2023-03-16 23:15:30 +0530

    EP changes

--- ep/src/config.py
+++ ep/src/config.py
@@ -3,3 +3,4 @@ from pathlib import Path
 trading_ = Path("/Users/yash/Desktop/Trading")
 ep = trading_ / "EP"
 ep_wl = ep / "watchlists"
+gap_data = ep / "gap"
---
+++ gsheets_finhubb/src/config.py
@@ -0,0 +1,5 @@
+from pathlib import Path
+
+trading_ = Path("/Users/yash/Desktop/Trading")
+ep = trading_ / "EP"
+ep_data_fh = ep / "finnhub"
